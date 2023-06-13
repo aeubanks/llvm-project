95682dbe11d76a3342870437377216e96b167504
Arthur Eubanks <aeubanks@google.com>
2023-06-13 14:06:16 -0700

    [MemorySSA] Always perform MemoryUses liveOnEntry optimization on MSSA construction
    Fixes invariant memory regressions in future DSE patches.
    
    Also add a flag to print<memoryssa> to not ensure optimized uses to test this.
    
    Noticeable compile time regression [1], but a future DSE change that depends on this more than makes up for it.
    
    [1] https://llvm-compile-time-tracker.com/compare.php?from=eb3d21be37c0b51d8e105a8c563626dd2b7e547d&to=5650772b87950a95dffd183e02368debb1056f99&stat=instructions:u
    
    Differential Revision: https://reviews.llvm.org/D152859

--- llvm/include/llvm/Analysis/MemorySSA.h
+++ llvm/include/llvm/Analysis/MemorySSA.h
@@ -946,9 +946,11 @@ public:
 /// Printer pass for \c MemorySSA.
 class MemorySSAPrinterPass : public PassInfoMixin<MemorySSAPrinterPass> {
   raw_ostream &OS;
+  bool EnsureOptimizedUses;
 
 public:
-  explicit MemorySSAPrinterPass(raw_ostream &OS) : OS(OS) {}
+  explicit MemorySSAPrinterPass(raw_ostream &OS, bool NoEnsureOptimizedUses)
+      : OS(OS), EnsureOptimizedUses(NoEnsureOptimizedUses) {}
 
   PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
 };
